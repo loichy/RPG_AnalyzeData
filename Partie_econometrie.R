ea95c36d759742ab5d2f803a2482092ddbbfd294
nathaliegogniokane <nathaliegogniokane@gmail.com>
2026-06-16 14:14:57 +0200

    +2 régressions, doc html

--- Partie_econometrie.R
+++ Partie_econometrie.R
@@ -198,10 +198,14 @@ ggplot(df_basque, aes(x = year, y = R99p)) +
 
 # ==== Building the indicators for future models ====
 
+# Bins created in the function directly !
 
-# 1. WE build 3°C GSDD bins:
+# We want to control for total annual precipitation
+# We multiply by 1000 to have the evolution in mm instead of m
 
-df_pra <- df_pra_yearly 
+df_pra_yearly$total_precip_wet_days_mm <- 1000*df_pra_yearly$total_precip_wet_days
+
+df_pra <- df_pra_yearly
 
 # We define the lower bounds: to go until gsdd_bin_24_27
 
@@ -224,13 +228,8 @@ for (i in bornes_inf) {
 
 # Special case for the last bin: 27_more
 
-df_pra$gsdd_bin_27_more <- df_pra$gsdd_above27 
+df_pra$gsdd_bin_27_more <- df_pra$gsdd_above27
 
-
-# We want to control for total annual precipitation
-# We multiply by 1000 to have the evolution in mm instead of m
-
-df_pra$total_precip_wet_days_mm <- 1000*df_pra$total_precip_wet_days
 
 mon_dico <- c(
   "gsdd_bin_00_03"  = "Tranche GSDD [0°C - 3°C]",
@@ -243,10 +242,15 @@ mon_dico <- c(
   "gsdd_bin_21_24" = "Tranche GSDD [21°C - 24°C]",
   "gsdd_bin_24_27" = "Tranche GSDD [24°C - 27°C]",
   "gsdd_bin_27_more" = "Tranche GSDD [+27°C]",
+  "gsdd_bin_00_10" = "Tranche GSDD [0°C - 10°C]",
+  "gsdd_bin_10_26" = "Tranche GSDD [10°C - 26°C]",
+  "gsdd_bin_26_more" = "Tranche GSDD [+26°C]",
   "total_precip_wet_days_mm" = "PRCP totales annuelles",
   "total_precip_wet_days_mm2" = "(PRCP totales annuelles)$^2$",
   "gsl_days" = "GSL",
-  "gsl_start_doy" = "1st GSL doy"
+  "gsl_start_doy" = "1st GSL doy",
+  "year" = "Année",
+  "code_insee" = "PRA"
 )
 
 # ==== Descriptive statistics ====
@@ -323,8 +327,10 @@ df <- df_pra %>%
 # We list all the columns that start with gsdd_bin_
 les_bins <- grep("^gsdd_bin_", names(df), value = TRUE)
 
-# We will want to control for precipitations
+# We will want to control for precipitations 
 df$total_precip_wet_days_mm2 <- df$total_precip_wet_days_mm^2
+
+controls <- c("total_precip_wet_days_mm", "total_precip_wet_days_mm2")
 
 # We create the formula: "surf_code_group_m2 ~ gsdd_bin_0_3 + gsdd_bin_03_06 + ..."
 
@@ -335,54 +341,33 @@ reg_surf_mean <- feols(
 # We add year fixed effects
 
 fe_surf_mean1 <- feols(
-  log(surf_code_group_m2/10000) ~ gsdd_bin_00_03 + gsdd_bin_03_06 + gsdd_bin_06_09 
-  + gsdd_bin_09_12 + gsdd_bin_12_15 + gsdd_bin_15_18 + gsdd_bin_18_21 + 
-    gsdd_bin_21_24 + gsdd_bin_24_27 + gsdd_bin_27_more | year, df)
+  as.formula(paste("log(surf_code_group_m2/10000) ~", paste(les_bins, collapse = " + "), "| year")), 
+  df)
 
 # And PRA fixed effects
 
 fe_surf_mean2 <- feols(
-  log(surf_code_group_m2/10000) ~ gsdd_bin_00_03 + gsdd_bin_03_06 + gsdd_bin_06_09 
-  + gsdd_bin_09_12 + gsdd_bin_12_15 + gsdd_bin_15_18 + gsdd_bin_18_21 + 
-    gsdd_bin_21_24 + gsdd_bin_24_27 + gsdd_bin_27_more | year + code_insee, df)
+  as.formula(paste("log(surf_code_group_m2/10000) ~", paste(les_bins, collapse = " + "), "| year + code_insee")),
+  df)
 
 
-# We add precipitation and potentiel yields controls
+# We add precipitation controls
 
 fe_surf_mean3 <- feols(
-  log(surf_code_group_m2/10000) ~ gsdd_bin_00_03 + gsdd_bin_03_06 + gsdd_bin_06_09 
-  + gsdd_bin_09_12 + gsdd_bin_12_15 + gsdd_bin_15_18 + gsdd_bin_18_21 + 
-    gsdd_bin_21_24 + gsdd_bin_24_27 + gsdd_bin_27_more + 
-    total_precip_wet_days_mm + total_precip_wet_days_mm2 + change_group | year + code_insee, df)
+  as.formula(paste("log(surf_code_group_m2/10000) ~", paste(les_bins, collapse = " + "), 
+                   "+", paste(controls, collapse = " + "), "| year + code_insee")),
+  df)
 
-# fe_surf_mean4 <- feols(
-#   log(surf_code_group_m2/10000) ~ gsdd_bin_00_03 + gsdd_bin_03_06 + gsdd_bin_06_09 
-#   + gsdd_bin_09_12 + gsdd_bin_12_15 + gsdd_bin_15_18 + gsdd_bin_18_21 + 
-#     gsdd_bin_21_24 + gsdd_bin_24_27 + gsdd_bin_27_more + change_group | year + code_insee, df)
+# We add potential yield controls
 
+fe_surf_mean4 <- feols(
+  as.formula(paste("log(surf_code_group_m2/10000) ~", paste(les_bins, collapse = " + "), 
+                   "+", paste(controls, collapse = " + "), "+ i(year, change_group)", "| year + code_insee")),
+  df)
 
-mon_dico <- c(
-  "gsdd_bin_00_03"  = "Tranche GSDD [0°C - 3°C]",
-  "gsdd_bin_03_06" = "Tranche GSDD [3°C - 6°C]",
-  "gsdd_bin_06_09" = "Tranche GSDD [6°C - 9°C]",
-  "gsdd_bin_09_12" = "Tranche GSDD [9°C - 12°C]",
-  "gsdd_bin_12_15" = "Tranche GSDD [12°C - 15°C]",
-  "gsdd_bin_15_18" = "Tranche GSDD [15°C - 18°C]",
-  "gsdd_bin_18_21" = "Tranche GSDD [18°C - 21°C]",
-  "gsdd_bin_21_24" = "Tranche GSDD [21°C - 24°C]",
-  "gsdd_bin_24_27" = "Tranche GSDD [24°C - 27°C]",
-  "gsdd_bin_27_more" = "Tranche GSDD [+27°C]",
-    "(Intercept)"    = "Constante",
-  "log(surf_code_group_m2/10000)" = paste0("Log(Surface de ", nom, " en ha)"),
-  "year" = "Année",
-  "code_insee" = "PRA",
-  "total_precip_wet_days_mm" = "PRCP totales annuelles",
-  "total_precip_wet_days_mm2" = "(PRCP totales annuelles)$^2$",
-  "change_group" = "Taux de croissance des rendements potentiels"
-)
 
 latex <- etable(reg_surf_mean, fe_surf_mean1, fe_surf_mean2, fe_surf_mean3,
-                # fe_surf_mean4,
+                fe_surf_mean4,
        dict = mon_dico,
        tex = F)
 
@@ -409,60 +394,43 @@ model1_bis <- function (code_group, nom, stat) {
   # We list all the columns that start with gsdd_bin_
   les_bins <- grep("^gsdd_bin_", names(df), value = TRUE)
   
+  # We will want to control for precipitations 
+  df$total_precip_wet_days_mm2 <- df$total_precip_wet_days_mm^2
+  
+  controls <- c("total_precip_wet_days_mm", "total_precip_wet_days_mm2")
+  
   # We create the formula: "surf_code_group_m2 ~ gsdd_bin_0_3 + gsdd_bin_03_06 + ..."
   reg_surf_mean <- feols(
     as.formula(paste("log(perc_group_m2) ~", paste(les_bins, collapse = " + "))), 
     df)
   
-  # We will want to control for precipitations 
-  df$total_precip_wet_days_mm2 <- df$total_precip_wet_days_mm^2
-  
   # We add year fixed effects
-  
   fe_surf_mean1 <- feols(
-    log(perc_group_m2) ~ gsdd_bin_00_03 + gsdd_bin_03_06 + gsdd_bin_06_09 
-    + gsdd_bin_09_12 + gsdd_bin_12_15 + gsdd_bin_15_18 + gsdd_bin_18_21 + 
-      gsdd_bin_21_24 + gsdd_bin_24_27 + gsdd_bin_27_more | year, df)
+      as.formula(paste("log(perc_group_m2) ~", paste(les_bins, collapse = " + "), "| year")), 
+      df)
   
   # And PRA fixed effects
-  
   fe_surf_mean2 <- feols(
-    log(perc_group_m2) ~ gsdd_bin_00_03 + gsdd_bin_03_06 + gsdd_bin_06_09 
-    + gsdd_bin_09_12 + gsdd_bin_12_15 + gsdd_bin_15_18 + gsdd_bin_18_21 + 
-      gsdd_bin_21_24 + gsdd_bin_24_27 + gsdd_bin_27_more | year + code_insee, df)
+    as.formula(paste("log(perc_group_m2) ~", paste(les_bins, collapse = " + "), "| year + code_insee")),
+    df)
   
-  # We add precipitation and potentiel yields controls
-  
+  # We add precipitation controls
   fe_surf_mean3 <- feols(
-    log(perc_group_m2) ~ gsdd_bin_00_03 + gsdd_bin_03_06 + gsdd_bin_06_09 
-    + gsdd_bin_09_12 + gsdd_bin_12_15 + gsdd_bin_15_18 + gsdd_bin_18_21 + 
-      gsdd_bin_21_24 + gsdd_bin_24_27 + gsdd_bin_27_more + 
-      total_precip_wet_days_mm + total_precip_wet_days_mm2 + change_group | year + code_insee, df)
+    as.formula(paste("log(perc_group_m2) ~", paste(les_bins, collapse = " + "), 
+                     "+", paste(controls, collapse = " + "), "| year + code_insee")),
+    df)
   
+  # We add potential yield controls
   
-  mon_dico <- c(
-    "gsdd_bin_00_03"  = "Tranche GSDD [0°C - 3°C]",
-    "gsdd_bin_03_06" = "Tranche GSDD [3°C - 6°C]",
-    "gsdd_bin_06_09" = "Tranche GSDD [6°C - 9°C]",
-    "gsdd_bin_09_12" = "Tranche GSDD [9°C - 12°C]",
-    "gsdd_bin_12_15" = "Tranche GSDD [12°C - 15°C]",
-    "gsdd_bin_15_18" = "Tranche GSDD [15°C - 18°C]",
-    "gsdd_bin_18_21" = "Tranche GSDD [18°C - 21°C]",
-    "gsdd_bin_21_24" = "Tranche GSDD [21°C - 24°C]",
-    "gsdd_bin_24_27" = "Tranche GSDD [24°C - 27°C]",
-    "gsdd_bin_27_more" = "Tranche GSDD [+27°C]",
-    "(Intercept)"    = "Constante",
-    "log(perc_group_m2)" = paste0("Log(Surface de ", nom, " en %)"),
-    "year" = "Année",
-    "code_insee" = "PRA", 
-    "total_precip_wet_days_mm" = "PRCP totales annuelles",
-    "total_precip_wet_days_mm2" = "(PRCP totales annuelles)$^2$",
-    "change_group" = "Taux de croissance des rendements potentiels"
-  )
+  fe_surf_mean4 <- feols(
+    as.formula(paste("log(perc_group_m2) ~", paste(les_bins, collapse = " + "), 
+                     "+", paste(controls, collapse = " + "), "+ i(year, change_group)", "| year + code_insee")),
+    df)
   
   latex <- etable(reg_surf_mean, fe_surf_mean1, fe_surf_mean2, fe_surf_mean3,
+                  fe_surf_mean4,
                   dict = mon_dico,
-                  tex = T)
+                  tex = F)
   
   return(latex)
 }
@@ -522,7 +490,7 @@ model2 <- function (code_group, nom, stat) {
   
   latex <- etable(reg_surf_mean, fe_surf_mean1, fe_surf_mean2, fe_surf_mean3,
                   dict = mon_dico,
-                  tex = T)
+                  tex = F)
   
   return(latex)
 }
@@ -581,7 +549,7 @@ model2_bis <- function (code_group, nom, stat) {
   
   latex <- etable(reg_surf_mean, fe_surf_mean1, fe_surf_mean2, fe_surf_mean3,
                   dict = mon_dico,
-                  tex = T)
+                  tex = F)
   
   return(latex)
 }
@@ -594,3 +562,191 @@ model2_bis("2", "Maïs", "daily_mean")
 
 model2_bis("21", "Vignes", "daily_mean")
 
+
+# ==== Model n°3 : Planted_surface = B*GSDD(non linear function) + ... + epsilon ====
+
+# We will tend to use the daily_maximum here
+
+# 1. Surface in ha
+
+model3 <- function (code_group, nom, stat, breaks) {
+  
+  df <- df_pra_yearly %>%
+    filter(CODE_GROUP == code_group, statistic.x == stat)
+  
+  # We define the lower bounds: to go until gsdd_bin_26_more
+  
+  # Loop to create the bins 
+  
+  # Loop to create the bins between breakpoints
+  for (i in seq_along(breaks)) {
+    inf <- breaks[i]
+    sup <- if (i < length(breaks)) breaks[i + 1] else NULL
+    
+    col_inf <- paste0("gsdd_above", sprintf("%02d", inf))
+    
+    if (!is.null(sup)) {
+      # Standard bin between two breakpoints
+      col_sup <- paste0("gsdd_above", sprintf("%02d", sup))
+      nom_bin <- paste0("gsdd_bin_", sprintf("%02d", inf), "_", sprintf("%02d", sup))
+      df[[nom_bin]] <- df[[col_inf]] - df[[col_sup]]
+    } else {
+      # Last bin: above the last breakpoint
+      nom_bin <- paste0("gsdd_bin_", sprintf("%02d", inf), "_more")
+      df[[nom_bin]] <- df[[col_inf]]
+    }
+  }
+  
+  # 1 ha = 10 000m2
+  
+  # We list all the columns that start with gsdd_bin_
+  les_bins <- grep("^gsdd_bin_", names(df), value = TRUE)
+  
+  # We will want to control for precipitations
+  df$total_precip_wet_days_mm2 <- df$total_precip_wet_days_mm^2
+  
+  # We define the controls we want to set
+  
+  controls <- c("total_precip_wet_days_mm", "total_precip_wet_days_mm2")
+  
+  # We create the formula: "surf_code_group_m2 ~ gsdd_bin_0_3 + gsdd_bin_03_06 + ..."
+  
+  reg_surf_mean <- feols(
+    as.formula(paste("log(surf_code_group_m2/10000) ~", paste(les_bins, collapse = " + "))), 
+    df)
+  
+  # We add year fixed effects
+  
+  fe_surf_mean1 <- feols(
+    as.formula(paste("log(surf_code_group_m2/10000) ~", paste(les_bins, collapse = " + "), "| year")), 
+    df)
+  
+  # And PRA fixed effects
+  
+  fe_surf_mean2 <- feols(
+    as.formula(paste("log(surf_code_group_m2/10000) ~", paste(les_bins, collapse = " + "), "| year + code_insee")),
+    df)
+  
+  
+  # We add precipitation and potentiel yields controls
+  
+  fe_surf_mean3 <- feols(
+    as.formula(paste("log(surf_code_group_m2/10000) ~", paste(les_bins, collapse = " + "), 
+                     "+", paste(controls, collapse = " + "), "| year + code_insee")),
+    df)
+  
+  # We add potential yield controls
+  
+  fe_surf_mean4 <- feols(
+    as.formula(paste("log(surf_code_group_m2/10000) ~", paste(les_bins, collapse = " + "), 
+                     "+", paste(controls, collapse = " + "), "+ i(year, change_group)", "| year + code_insee")),
+    df)
+  
+  latex <- etable(reg_surf_mean, fe_surf_mean1, fe_surf_mean2, fe_surf_mean3,
+                  fe_surf_mean4,
+                  dict = mon_dico,
+                  tex = F)
+  
+  return(latex)
+}
+
+model3("8", "Protéagineux", "daily_maximum", c(0, 10, 26))
+
+model3("1", "Blé", "daily_maximum", c(0, 10, 26))
+
+model3("2", "Maïs", "daily_maximum", c(0, 10, 26))
+
+model3("21", "Vignes", "daily_maximum", c(0, 10, 26))
+
+# 1. Surface in ha
+
+model3_bis <- function (code_group, nom, stat, breaks) {
+  
+  df <- df_pra_yearly %>%
+    filter(CODE_GROUP == code_group, statistic.x == stat)
+  
+  # We define the lower bounds: to go until gsdd_bin_26_more
+  
+  # Loop to create the bins 
+  
+  # Loop to create the bins between breakpoints
+  for (i in seq_along(breaks)) {
+    inf <- breaks[i]
+    sup <- if (i < length(breaks)) breaks[i + 1] else NULL
+    
+    col_inf <- paste0("gsdd_above", sprintf("%02d", inf))
+    
+    if (!is.null(sup)) {
+      # Standard bin between two breakpoints
+      col_sup <- paste0("gsdd_above", sprintf("%02d", sup))
+      nom_bin <- paste0("gsdd_bin_", sprintf("%02d", inf), "_", sprintf("%02d", sup))
+      df[[nom_bin]] <- df[[col_inf]] - df[[col_sup]]
+    } else {
+      # Last bin: above the last breakpoint
+      nom_bin <- paste0("gsdd_bin_", sprintf("%02d", inf), "_more")
+      df[[nom_bin]] <- df[[col_inf]]
+    }
+  }
+  
+  # 1 ha = 10 000m2
+  
+  # We list all the columns that start with gsdd_bin_
+  les_bins <- grep("^gsdd_bin_", names(df), value = TRUE)
+  
+  # We will want to control for precipitations
+  df$total_precip_wet_days_mm2 <- df$total_precip_wet_days_mm^2
+  
+  # We define the controls we want to set
+  
+  controls <- c("total_precip_wet_days_mm", "total_precip_wet_days_mm2")
+  
+  # We create the formula: "perc_group_m2 ~ gsdd_bin_... + ..."
+  
+  reg_surf_mean <- feols(
+    as.formula(paste("log(perc_group_m2) ~", paste(les_bins, collapse = " + "))), 
+    df)
+  
+  # We add year fixed effects
+  
+  fe_surf_mean1 <- feols(
+    as.formula(paste("log(perc_group_m2) ~", paste(les_bins, collapse = " + "), "| year")), 
+    df)
+  
+  # And PRA fixed effects
+  
+  fe_surf_mean2 <- feols(
+    as.formula(paste("log(perc_group_m2) ~", paste(les_bins, collapse = " + "), "| year + code_insee")),
+    df)
+  
+  
+  # We add precipitation controls
+  
+  fe_surf_mean3 <- feols(
+    as.formula(paste("log(perc_group_m2) ~", paste(les_bins, collapse = " + "), 
+                     "+", paste(controls, collapse = " + "), "| year + code_insee")),
+    df)
+  
+  # We add potential yield controls
+  
+  fe_surf_mean4 <- feols(
+    as.formula(paste("log(perc_group_m2) ~", paste(les_bins, collapse = " + "), 
+                     "+", paste(controls, collapse = " + "), "+ i(year, change_group)", "| year + code_insee")),
+    df)
+
+  
+  
+  latex <- etable(reg_surf_mean, fe_surf_mean1, fe_surf_mean2, fe_surf_mean3,
+                  fe_surf_mean4,
+                  dict = mon_dico,
+                  tex = F)
+  
+  return(latex)
+}
+
+model3_bis("8", "Protéagineux", "daily_maximum", c(0, 10, 26))
+
+model3_bis("1", "Blé", "daily_maximum", c(0, 10, 26))
+
+model3_bis("2", "Maïs", "daily_maximum", c(0, 10, 26))
+
+model3_bis("21", "Vignes", "daily_maximum", c(0, 10, 26))
